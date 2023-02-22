84987a8c56570b57749fa651b4976d6bd1ff13aa
Cherniakov_Pavel <74683152+Pavel5284@users.noreply.github.com>
2023-02-22 18:19:27 +0200

    add.post

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ let smtp_password = process.env.SMTP_PASSWORD
         res.send('Hello World!')
     })
 
-    app.get('/sendMessage', async function (req, res) {
+    app.post('/sendMessage', async function (req, res) {
         let{name, email, subject, message} = req.body
 
 
